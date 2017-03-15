0887b77e53c60be10233348989588be119521619
pvosper <pvosper@mac.com>
2017-03-14 21:00:08 -0700

    WIP: Adding gunicorn file

--- production.py
+++ production.py
@@ -2,5 +2,5 @@ from .settings import *
 
 DEBUG = False
 TEMPLATE_DEBUG = False
-ALLOWED_HOSTS = ['<your instance public dns>', 'localhost']
+ALLOWED_HOSTS = ['ec2-52-40-183-184.us-west-2.compute.amazonaws.com', 'localhost']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
